d3e6910db2e36ed7ac605199e414ac612c48247b
Michelle <mtham101@gmail.com>
2016-11-02 17:34:29 -0700

    [feat] allow user to post new entries

--- public/models.py
+++ public/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -12,31 +11,19 @@ from django.contrib.auth.models import User
 # models.Model tells Django that Post is a Django Model
 class Post(models.Model):
   author = models.ForeignKey(User)
-  slug = models.SlugField(unique=True)
   title = models.CharField(max_length=200)
   text = models.TextField()
   created_date = models.DateTimeField(default=timezone.now)
   published_date = models.DateTimeField(blank=True, null=True)
-  # def publish(self):
-  #   self.published_date = timezone.now()
-  #   self.save()
+  def publish(self):
+    self.published_date = timezone.now()
+    self.save()
   def __unicode__(self):
     return self.title
-  @models.permalink
-  def get_absolute_url(self):
-    return ('blog_post_detail', (),
-            {
-                'slug' :self.slug,
-            })
-  def save(self, *args, **kwargs):
-    if not self.slug:
-        self.slug = slugify(self.title)
-    super(Post, self).save(*args, **kwargs)
 
 # Model to post comments
 class Comment(models.Model):
   name = models.CharField(max_length=42)
-  text = models.CharField(max_length=200)
   text = models.TextField()
   post = models.ForeignKey(Post)
   created_date = models.DateTimeField(default=timezone.now)
